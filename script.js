b912eb95b2cfba64b9887da41998ee01e487d7a1
saloni mourya <manshimaurya195@gmail.com>
2026-02-04 13:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
+let form = document.querySelector("#form1")
+form.addEventListener("submit",getFormvalue)
+
+function getFormvalue(e) {
     //Write your code here
+	e.preventDefault()
+	let fname = document.querySelector('input[name="fname"]').value;
+	let lname = document.querySelector('input[name="lname"]').value;
+alert(fname +" " +lname)	
 
 }
